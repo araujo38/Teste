e779f341b2f89adda63247db29db491073284081
bpm1993 <bpm1993@icloud.com>
2015-11-03 20:59:35 -0200

    Atualização no ToDo

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,4 +1,27 @@
 
 public class ToDo {
 
+	/*
+	 * João:
+	 * Criar Inimigo Kamikaze
+	 * Criar Inimigo Atirador
+	 * 
+	 * Camila:
+	 * Implementar A* pro Kamikaze
+	 * Implementar A* pro Atirador
+	 * Implementar Fuzzy para nivelar dificuldade
+	 * 
+	 * Bruno:
+	 * Organizar Código orientado a objeto
+	 * Criar Inimigo Space Invader
+	 * Criar Menu
+	 * Criar Pontuação
+	 * Criar HP
+	 * Criar Conquistas
+	 * 
+	 * Fabiano:
+	 * Criar Background
+	 * Implementar imagem inimigos
+	 */
+	
 }
